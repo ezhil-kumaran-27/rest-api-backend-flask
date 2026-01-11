5abaf6c1ba326f11480b44cd98ea633de3e19d73
ezhil-kumaran-27 <ezhilkumaranmanikandans@gmail.com>
2026-01-11 21:11:33 +0530

    make the login and profile api in auth file

--- routes/auth.py
+++ routes/auth.py
@@ -1,13 +1,16 @@
-from flask import request, Blueprint
+from flask import request, Blueprint,jsonify
 from models.user import User
 from database.db import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required, get_jwt_identiity
 
-auth_bp=Blueprint("auth",__name__)
+auth_bp = Blueprint("auth",__name__)
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
-    data = request.json
-
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'])
     user = User(
         email=data['email'],
         password = data['password']
@@ -15,4 +18,32 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    return{'message': "User Registered Successfully"},201
+    return jsonify({'message': "User Registered Successfully"}),201
+
+@auth_bp.route("/login",methods = ["POST"])
+def login():
+    data = request.get_json()
+
+    user = User.query.filter_by(email=data['email']).first()
+
+    if not user or not check_password_hash(user.password,data["password"]):
+        return {"error": "Invalid email or password"},401
+    access_token = create_access_token(identity=user.id)
+
+    return{
+        "message":"Login successful",
+        "access_token":access_token
+
+    }
+
+@auth_bp.route("/profile",methods = ["GET"])
+@jwt_required()
+def profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    return{
+        "id": user.id,
+        "email": user.email,
+
+    }
