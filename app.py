e0b7ac802bd50aec66fd7aec627443e6b81c7a4b
ezhil-kumaran-27 <ezhilkumaranmanikandans@gmail.com>
2026-01-11 19:12:42 +0530

    Created Register  Blueprint in app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from database.db import db
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
-
+app.register_blueprint(auth_bp)
 db.init_app(app)
 @app.route('/')
 def home():
