d3396a702b12983bde3f8ed8535abcae9b5aca98
ezhil-kumaran-27 <ezhilkumaranmanikandans@gmail.com>
2026-01-11 21:13:31 +0530

    created the jwt_secret_key for password auth file

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
     SECRET_KEY = "mysecretkey"
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "jwt-secret-key"
